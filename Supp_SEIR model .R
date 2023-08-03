b3c292ccb706a69dc79cbb716a0a639f69486718
David Manheim <davidmanheim@gmail.com>
2023-08-03 11:59:06 +0300

    Supp_SEIR model.R - use loop
    Rewrite to loop through the cases instead of running them individually. Add case for MERS (with placeholder values)

--- Supp_SEIR model .R
+++ Supp_SEIR model .R
@@ -14,82 +14,47 @@ SEIR <- function(time, current_state, params){
   })
 }
 
-### Modify parameters for SARS-CoV-2 Wt and run ode
+#Replace individual runs with a list of what to run.
+Disease_Cases <- list(list(name="SARS-CoV-2", params=c(beta=0.32, sigma=0.15, gamma=0.125)),
+                      list(name="SARS-CoV-2 Omicron", params=c(beta=1.19, sigma=0.25, gamma=0.125)),
+                      list(name="SARS", params=c(beta=0.24, sigma=0.25, gamma=0.1)),
+                      list(name="Seasonal Flu", params=c(beta=0.33, sigma=0.5, gamma=0.25)),
+                      list(name="1918 Flu", params=c(beta=0.5, sigma=0.5, gamma=0.25)),
+                      list(name="MERS", params=c(beta=0.9, sigma=0.9, gamma=0.9))
+)
+Disease_names=c()
+for (i in 1:length(Disease_Cases)){
+  Disease_names=append(Disease_names,Disease_Cases[[i]]$name)
+}
 
-params <- c(beta=0.32, sigma=0.15, gamma=0.125)
-
+#Across all diseases:
 initial_state <- c(S=6500000, E=0, I=1, R=0)
-
 times <- 0:365
 
-model <- ode(initial_state, times, SEIR, params)
+model_df<-data.frame(times)
+for(i in 1:length(Disease_Cases)){
+  model <- ode(initial_state, times, SEIR, Disease_Cases[[i]]$params)
+  matplot(model, type="l", lty=1, main=paste("SEIR model",Disease_Cases[[i]]$name), xlab="Time")
+  legend <- colnames(model)[2:6]
+  model_df<-cbind(model_df,data.frame(model))
+}
 
-matplot(model, type="l", lty=1, main="SEIR model", xlab="Time")
+#Get distinct names per case:
+colnames(model_df) <- make.names(colnames(model_df), unique = TRUE)
 
-legend <- colnames(model)[2:6]
-
-### Modify parameters for SARS-CoV-2 Omi and run ode
-
-params <- c(beta=1.19, sigma=0.25, gamma=0.125)
-
-initial_state <- c(S=6500000, E=0, I=1, R=0)
-
-model_2 <- ode(initial_state, times, SEIR, params)
-
-matplot(model_2, type="l", lty=1, main="SEIR model", xlab="Time")
-
-legend <- colnames(model_2)[2:6]
-
-legend("right", legend=legend, col=2:6, lty = 1)
-
-### Modify parameters for SARS and run ode
-
-params <- c(beta=0.24, sigma=0.25, gamma=0.1)
-
-initial_state <- c(S=6500000, E=0, I=1, R=0)
-
-model_3 <- ode(initial_state, times, SEIR, params)
-
-matplot(model_3, type="l", lty=1, main="SEIR model", xlab="Time")
-
-legend <- colnames(model_3)[2:6]
-
-legend("right", legend=legend, col=2:6, lty = 1)
-
-### Modify parameters for Seasonal Influenza and run ode
-
-params <- c(beta=0.33, sigma=0.5, gamma=0.25)
-
-initial_state <- c(S=6500000, E=0, I=1, R=0)
-
-model_4 <- ode(initial_state, times, SEIR, params)
-
-matplot(model_4, type="l", lty=1, main="SEIR model", xlab="Time")
-
-legend <- colnames(model_4)[2:6]
-
-legend("right", legend=legend, col=2:6, lty = 1)
-
-### 1918 Influenza
-
-params <- c(beta=0.5, sigma=0.5, gamma=0.25)
-
-initial_state <- c(S=6500000, E=0, I=1, R=0)
-
-model_5 <- ode(initial_state, times, SEIR, params)
-
-matplot(model_5, type="l", lty=1, main="SEIR model", xlab="Time")
-
-legend <- colnames(model_5)[2:6]
-
-legend("right", legend=legend, col=2:6, lty = 1)
-
-#Collate data into a dataframe
-
-model_df<-data.frame(model,
-                     model_2,
-                     model_3,
-                     model_4,
-                     model_5)
-
-write_xlsx(model_df, "model.xlsx")
+#Group into cumulative infections
+model_df_aggregated<-data.frame(times)
+for(i in 1:length(Disease_Cases)){
+  case = i-1
+  if(case==0){
+    model_df_aggregated<-cbind(model_df_aggregated,model_df$I+model_df$R)
+  }
+  else{
+    model_df_aggregated<-cbind(model_df_aggregated,
+                               get(paste0('I.',case), envir=as.environment(model_df)) +
+                               get(paste0('R.',case), envir=as.environment(model_df))
+    )
+  }
+  
+}
+names(model_df_aggregated) <- c("time",Disease_names)
