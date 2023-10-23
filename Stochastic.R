15ed355171a007827bfef601be572be1d9c6f33f
Oscar Delaney <o.delaney@uq.net.au>
2023-10-23 17:35:02 +1000

    make the stochastic implementation better by: adding graphing functionality parallelising properly allowing a detection threshold

--- Stochastic.R
+++ Stochastic.R
@@ -5,63 +5,68 @@ library(future.apply)
 library(tidyverse)
 
 # Define the SEIR transitions
-transitions = list(
-  c(S = -1, E = 1),               # Infection
-  c(E = -1, I = 1),               # Progression to infectious
-  c(I = -1, R = 1)                # Recovery
+transitions <- list(
+  c(S = -1, E = 1), # Infection
+  c(E = -1, I = 1), # Progression to infectious  
+  c(E = -1, I = +1, D = +1), # Detection
+  c(I = -1, R = 1), # Recovery
+  c(D = +1) # Outbreak alerted
 )
 
 # Function to calculate transition rates for SEIR
 SEIRrates <- function(x, params, t) {
-  N <- sum(x)
-  lambda_SE <- (params["beta"] * x["S"] * x["I"]) / N
-  lambda_EI <- params["sigma"] * x["E"]
-  lambda_IR <- params["gamma"] * x["I"]
-  
-  return(c(lambda_SE, lambda_EI, lambda_IR))
-}
-
-
-# Run stochastic SEIR for a given disease
-run_SEIR <- function(disease_name, rep = 1) {
-  params <- Disease_Cases[[which(Disease_names == disease_name)]]$params
-  plan(multisession)
-  results <- bind_rows(future_lapply(1:rep, function(x) {
-    out <- ssa.adaptivetau(initial_state, transitions, SEIRrates, params, tf = max(times))
-    df <- as_tibble(out)
-    return(df)
-  }, future.seed = TRUE))
-  return(df)
+  with(as.list(c(params, x)), {
+    N <- sum(x)
+    SE <- beta * S * I / N
+    EI <- sigma * E
+    IR <- gamma * I
+    return(c(SE, EI, EI * p, IR, ifelse(D >= threshold, 1e9, 0)))
+  })
 }
 
 # Run stochastic SEIR for a given disease multiple times using parallelization
-run_SEIR <- function(disease_name, n_times=100) {
+run_SEIR <- function(disease_name, rep=100, p=0.01, threshold = 1,
+  initial_state = c(S = 6500000, E = 0, I = 1, R = 0, D = 0), time = 100) {
   params <- Disease_Cases[[which(Disease_names == disease_name)]]$params
-  results <- pivot_longer(bind_rows(future_lapply(1:n_times, function(x) {
-    out <- ssa.adaptivetau(initial_state, transitions, SEIRrates, params, tf = max(times))
-    tibble_data <- as_tibble(out) %>%
-      mutate(rep = x) # add replication number
+  params["p"] <- p
+  params["threshold"] <- threshold
+  plan(multisession)
+  results <- pivot_longer(bind_rows(future_lapply(1:rep, function(i) {
+    out <- ssa.adaptivetau(initial_state, transitions, SEIRrates, params,
+      tf = time, halting = 5)
+    tibble_data <- as_tibble(out$dynamics) %>%
+      mutate(rep = i, cum_I = I + R, halted = out$haltingTransition) # add replication number
     return(tibble_data)
-  }, future.seed = TRUE)), cols = -c(time, rep), names_to = "variable")
+  }, future.seed = TRUE)), cols = -c(time, rep, halted), names_to = "variable")
 }
 
-
-# Your existing data
-Disease_Cases <- list(
-  list(name = "SARS-CoV-2", params = c(beta = 0.32, sigma = 0.15, gamma = 0.125)),
-  list(name = "SARS-CoV-2 Omicron", params = c(beta = 1.19, sigma = 0.25, gamma = 0.125)),
-  list(name = "SARS", params = c(beta = 0.24, sigma = 0.25, gamma = 0.1)),
-  list(name = "Seasonal Flu", params = c(beta = 0.33, sigma = 0.5, gamma = 0.25)),
-  list(name = "Pandemic 1918 Flu", params = c(beta = 0.5, sigma = 0.5, gamma = 0.25)),
-  list(name = "MERS", params = c(beta = 0.11, sigma = 0.18, gamma = 0.13))
-)
-Disease_names <- sapply(Disease_Cases, function(x) x$name)
-
-initial_state <- c(S = 6500000, E = 0, I = 1, R = 0)
-times <- 0:365
-
-# Setting up parallelization
-plan(multisession)
+# plot results
+plot_SEIR <- function(data) {
+  last <- data %>%
+    group_by(rep) %>%
+    slice_tail(n = 1)
+  ggplot(data, aes(x = time, y = value, group = interaction(variable, rep)
+  # , color = factor(variable, levels = c("S", "E", "I", "R", "cum_I"))
+  )) +
+    geom_line(linewidth = 1.5) +
+    geom_point(data = last, aes(x = time, y = value), size = 8, color = "red") +
+    labs(
+      x = "Days",
+      y = "Cumulative infections",
+      color = "Compartment"
+    ) +
+    scale_color_brewer(palette = "Set1") +
+    theme_light() +
+    theme(
+      legend.position = "none",
+      axis.title = element_text(size = 35),
+      axis.text = element_text(size = 25),
+      legend.title = element_text(size = 20),
+      legend.text = element_text(size = 20),
+      legend.key.size = unit(1.5, "cm")
+    )
+}
 
 # Example usage:
-data_for_SARS_CoV_2 <- run_SEIR("SARS-CoV-2", 2)
+# data <- run_SEIR("SARS-CoV-2", rep = rep)
+# plot_SEIR(data[data$variable == "cum_I" & !is.na(data$halted), ])
