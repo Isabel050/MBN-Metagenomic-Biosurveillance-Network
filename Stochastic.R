638b40ca227036baf35ebc5620b6ec84c210a1ee
Oscar-Delaney <o.delaney@uq.net.au>
2023-10-28 19:29:03 +1000

    make tibble with cum_I only by default include det and stoch in same tibble

--- Stochastic.R
+++ Stochastic.R
@@ -37,67 +37,58 @@ SEIR_ode <- function(t, y, params) {
 }
 
 # Run stochastic SEIR for a given disease multiple times using parallelization
-run_SEIR <- function(disease_name, rep=100, p=0.01, threshold = 1,
-  initial_state = c(S = 6500000, E = 0, I = 1, R = 0, D = 0), time = 100, deterministic = FALSE) {
+run_SEIR <- function(disease_name, rep = 100, p = 0.01, threshold = 1,
+  initial_state = c(S = 6500000, E = 0, I = 1, R = 0, D = 0), time = 100) {
+
   params <- Disease_Cases[[which(Disease_names == disease_name)]]$params
   params["p"] <- p
   params["threshold"] <- threshold
   
-  if (deterministic) {
-    results <- data.frame(time = seq(0, time, by = 1))
-    out <- ode(y = initial_state[1:4], times = results$time, func = SEIR_ode, parms = params)
-    results <- cbind(results, out[, 2:5])
-    results <- results %>%
-      mutate(rep = 1, cum_I = I + R, halted = NA) %>%
-      pivot_longer(cols = -c(time, rep, halted), names_to = "variable", values_to = "value")
-  } else {
-    plan(multisession)
-    results <- pivot_longer(bind_rows(future_lapply(1:rep, function(i) {
-      out <- ssa.adaptivetau(initial_state, transitions, SEIRrates, params, tf = time, halting = 5)
-      tibble_data <- as_tibble(out$dynamics) %>%
-        mutate(rep = i, cum_I = I + R, halted = out$haltingTransition) # add replication number
-      return(tibble_data)
-    }, future.seed = TRUE)), cols = -c(time, rep, halted), names_to = "variable")
-  }
-
-  return(results)
+  # Deterministic simulation
+  out_det <- ode(y = initial_state[1:4], times = seq(0, time, by = 1), func = SEIR_ode, parms = params)
+  results_det <- tibble(
+    time = out_det[, "time"],
+    cum_I = out_det[, "I"] + out_det[, "R"],
+    rep = 0,
+    halted = NA
+  )
+  
+  # Stochastic simulations
+  plan(multisession)
+  results_sto <- bind_rows(future_lapply(1:rep, function(i) {
+    out <- ssa.adaptivetau(initial_state, transitions, SEIRrates, params, tf = time, halting = 5)
+    tibble_data <- as_tibble(out$dynamics) %>%
+      mutate(rep = i, cum_I = I + R, halted = out$haltingTransition)
+  }, future.seed = TRUE))
+  
+  return(bind_rows(results_det, results_sto))
 }
 
-plot_SEIR <- function(data, deterministic_data = NULL) {
+plot_SEIR <- function(data) {
   # Base plot
-  p <- ggplot() +
-       theme_light() +
+  p <- ggplot(data, aes(x = time, y = cum_I, group = rep)) +
+       geom_line(aes(color = rep != 0, size = rep != 0)) +
+       scale_size_manual(values = c(1.5, 0.5)) +
        labs(x = "Days since first case", y = "Cumulative infections until detection") +
        ylim(0, 500) +
+       theme_light() +
+       scale_color_manual(values = c("blue", "grey80")) +
        theme(
-         legend.position = "none",
          axis.title = element_text(size = 35),
          axis.text = element_text(size = 25),
-         legend.title = element_text(size = 20),
-         legend.text = element_text(size = 20),
-         legend.key.size = unit(1.5, "cm")
+         legend.position = "none"
        )
-  
-  # Add stochastic results if provided
-  if (!is.null(data)) {
-    last <- data %>%
+
+  # Highlight points where the outbreak is detected in stochastic runs
+  last <- data %>%
       group_by(rep) %>%
       filter(!is.na(halted)) %>%
       slice_tail(n = 1)
-    
-    p <- p + geom_line(data = data, aes(x = time, y = value, group = interaction(variable, rep)), color = "grey80", alpha = 1) +
-      geom_point(data = last, aes(x = time, y = value), size = 5, color = "red")
-  }
-  
-  # Add deterministic results if provided
-  if (!is.null(deterministic_data)) {
-    p <- p + geom_line(data = deterministic_data, aes(x = time, y = value, group = variable), size = 1.5)
-  }
+  p <- p + geom_point(data = last, aes(x = time, y = cum_I), size = 5, color = "red")
   
   return(p)
 }
 
 # Example usage:
-data_s <- run_SEIR("SARS-CoV-2", rep = 100, deterministic = F)
-data_d <- run_SEIR("SARS-CoV-2", rep = 1, deterministic = T)
-plot_SEIR(data_s[data_s$variable == "cum_I", ], data_d[data_d$variable == "cum_I", ])
+data <- run_SEIR("SARS-CoV-2", rep = 10)
+plot_SEIR(data)
